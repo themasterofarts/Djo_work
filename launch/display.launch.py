ef7153d9e69e8b0218a22adba38345073950e814
Djo9800 <siradjosy1@gmail.com>
2025-10-18 10:18:04 +0200

    modifif du code et rajout des fichier .xacro

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -5,7 +5,7 @@ import os, xacro
 
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
+from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
 
 def generate_launch_description():
     
@@ -14,25 +14,45 @@ def generate_launch_description():
     robot_desc = xacro.process_file(xacro_file).toxml()
     
     pkg_gazebo= get_package_share_directory('ros_gz_sim')
+
+
+    # gz_sim = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(pkg_gazebo, 'launch', 'gz_sim.launch.py'),
+    #     ),
+    #     launch_arguments={'gz_args': [PathJoinSubstitution([
+    #         pkg_path,
+    #         'worlds',
+    #         'djo_world.sdf'
+    #     ]),
+    #     TextSubstitution(text=' -r -v -v1 --render-engine ogre')],
+    #     #TextSubstitution(text=' -r -v -v1')],
+    #     'on_exit_shutdown': 'true'}.items()
+    #)
+
+
+    # gz_sim = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(pkg_gazebo, 'launch', 'gz_sim.launch.py'),
+    #     ),
+    #     launch_arguments={'gz_args': [PathJoinSubstitution([
+    #         pkg_path,
+    #         'worlds',
+    #         'empty_gz.sdf'
+    #     ]),
+    #     TextSubstitution(text=' -r -v -v1 --render-engine ogre')],
+    #     'on_exit_shutdown': 'true'}.items()
+    # )
     
-    #### Lancement de  ignition ######
-    ignition_gz= IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(pkg_gazebo,'launch','gz_sim.launch.py')),
-        launch_arguments={'worlds': PathJoinSubstitution([
-            pkg_gazebo,
-            'world',
-            'empty_ign.sdf']),
-                          
-            'extra_args': TextSubstitution(text=' -r -v -v1 --render-engine ogre')
-        }.items(),
-    )
 
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_gazebo, "launch", "gz_sim.launch.py")
         ),
         launch_arguments={
-            "gz_args": PathJoinSubstitution([pkg_path, "worlds", "empty_gz.world"])
+            "gz_args":[ PathJoinSubstitution([pkg_path, "worlds", "djo_world.sdf"]),
+             TextSubstitution(text=' -r -v -v1 --render-engine ogre')],
+
         }.items(),
     )
     
@@ -47,8 +67,8 @@ def generate_launch_description():
             parameters=[{'robot_description': robot_desc}]
         ),
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui'
         ),
         Node(
@@ -57,18 +77,51 @@ def generate_launch_description():
             name='rviz2',
             output='screen'
         ),
+
+
+        Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=[
+            "/camera/image",
+        ],
+        output="screen",
+        parameters=[
+            {'use_sim_time': True,
+             'camera.image.compressed.jpeg_quality': 75},
+        ],
+        ),
         
+         
         # Node(
-        #         package="ros_gz_bridge",
-        #         executable="parameter_bridge",
-        #         parameters=[
-        #             {
-        #                 "config_file": os.path.join(pkg_path, "config", "bridge.yaml"),
-        #                 "qos_overrides./tf_static.publisher.durability": "transient_local",
-        #             }
-        #         ],
-        #         output="screen",
-        #     ),
+        #     package='ros_gz_bridge',
+        #     executable='parameter_bridge',
+        #     parameters=[{
+        #         'config_file': os.path.join(pkg_path, 'configuration', 'bridge.yaml'),
+        #         'qos_overrides./tf_static.publisher.durability': 'transient_local',
+        #     }],
+        #     output='screen'
+        # ),
+
+       ### noeud du pont entre gazebo et ros pour la communication #####
+        Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry",
+            "/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model",
+            '/lidar/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'
+            "/tf@tf2_msgs/msg/TFMessage@gz.msgs.Pose_V",
+            #"/camera/image@sensor_msgs/msg/Image@gz.msgs.Image",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+            "imu@sensor_msgs/msg/Imu@gz.msgs.IMU",
+
+        ],
+        output="screen",
+        parameters=[{'use_sim_time': True    }    ]
+        ),
         
         ##  Faire apparaitre mon robot dans gazebo   ######
         Node(
@@ -94,6 +147,5 @@ def generate_launch_description():
                     "0.0",
                 ],
             ),
-        #ignition_gz,
         gz_sim,
     ])
